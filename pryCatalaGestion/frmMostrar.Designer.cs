40516a203597662c8ea117dffa2f1c29cdd4f921
valentinacatala <valenncatala@gmail.com>
2023-06-09 13:13:43 -0300

    09/06

--- pryCatalaGestion/frmMostrar.Designer.cs
+++ pryCatalaGestion/frmMostrar.Designer.cs
@@ -56,7 +56,7 @@
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(78, 35);
+            this.comboBox1.Location = new System.Drawing.Point(78, 39);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(210, 21);
             this.comboBox1.TabIndex = 3;
