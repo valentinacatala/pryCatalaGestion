9333b15033b93ba190b4daff5bcf9d7b6bd9dad6
Valentina Catalá <valenncatala@gmail.com>
2023-06-29 20:13:34 -0300

    29/6

--- pryCatalaGestion/frmBienvenida.Designer.cs
+++ pryCatalaGestion/frmBienvenida.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBienvenida));
             this.cmdRegistrar = new System.Windows.Forms.Button();
             this.cmdConsultar = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -37,9 +38,10 @@
             this.cmdRegistrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cmdRegistrar.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmdRegistrar.ForeColor = System.Drawing.Color.Teal;
-            this.cmdRegistrar.Location = new System.Drawing.Point(27, 65);
+            this.cmdRegistrar.Location = new System.Drawing.Point(36, 80);
+            this.cmdRegistrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmdRegistrar.Name = "cmdRegistrar";
-            this.cmdRegistrar.Size = new System.Drawing.Size(138, 73);
+            this.cmdRegistrar.Size = new System.Drawing.Size(184, 90);
             this.cmdRegistrar.TabIndex = 0;
             this.cmdRegistrar.Text = "Registrar Actividad";
             this.cmdRegistrar.UseVisualStyleBackColor = true;
@@ -50,9 +52,10 @@
             this.cmdConsultar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cmdConsultar.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmdConsultar.ForeColor = System.Drawing.Color.Teal;
-            this.cmdConsultar.Location = new System.Drawing.Point(200, 65);
+            this.cmdConsultar.Location = new System.Drawing.Point(267, 80);
+            this.cmdConsultar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmdConsultar.Name = "cmdConsultar";
-            this.cmdConsultar.Size = new System.Drawing.Size(138, 73);
+            this.cmdConsultar.Size = new System.Drawing.Size(184, 90);
             this.cmdConsultar.TabIndex = 1;
             this.cmdConsultar.Text = "Consultar Actividades";
             this.cmdConsultar.UseVisualStyleBackColor = true;
@@ -60,11 +63,13 @@
             // 
             // frmBienvenida
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(374, 199);
+            this.ClientSize = new System.Drawing.Size(499, 245);
             this.Controls.Add(this.cmdConsultar);
             this.Controls.Add(this.cmdRegistrar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmBienvenida";
--- pryCatalaGestion/frmMostrar.Designer.cs
+++ pryCatalaGestion/frmMostrar.Designer.cs
@@ -28,13 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMostrar));
             this.dgvMostrar = new System.Windows.Forms.DataGridView();
             this.dgvFecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dgvTipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dgvDetalle = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dgvReunion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dgvTareas = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblRegistroActividades = new System.Windows.Forms.Label();
             this.cmdIrARegistro = new System.Windows.Forms.Button();
             this.cmdCancelar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMostrar)).BeginInit();
@@ -42,6 +44,15 @@
             // 
             // dgvMostrar
             // 
+            this.dgvMostrar.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Impact", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvMostrar.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dgvMostrar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvMostrar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dgvFecha,
@@ -49,53 +60,62 @@
             this.dgvDetalle,
             this.dgvReunion,
             this.dgvTareas});
-            this.dgvMostrar.Location = new System.Drawing.Point(12, 54);
+            this.dgvMostrar.Location = new System.Drawing.Point(16, 66);
+            this.dgvMostrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvMostrar.Name = "dgvMostrar";
-            this.dgvMostrar.Size = new System.Drawing.Size(541, 294);
+            this.dgvMostrar.RowHeadersWidth = 51;
+            this.dgvMostrar.Size = new System.Drawing.Size(721, 362);
             this.dgvMostrar.TabIndex = 12;
             // 
             // dgvFecha
             // 
             this.dgvFecha.HeaderText = "Fecha";
+            this.dgvFecha.MinimumWidth = 6;
             this.dgvFecha.Name = "dgvFecha";
             // 
             // dgvTipo
             // 
             this.dgvTipo.HeaderText = "Tipo";
+            this.dgvTipo.MinimumWidth = 6;
             this.dgvTipo.Name = "dgvTipo";
             // 
             // dgvDetalle
             // 
             this.dgvDetalle.HeaderText = "Detalle";
+            this.dgvDetalle.MinimumWidth = 6;
             this.dgvDetalle.Name = "dgvDetalle";
             // 
             // dgvReunion
             // 
             this.dgvReunion.HeaderText = "Reunion";
+            this.dgvReunion.MinimumWidth = 6;
             this.dgvReunion.Name = "dgvReunion";
             // 
             // dgvTareas
             // 
             this.dgvTareas.HeaderText = "Tareas";
+            this.dgvTareas.MinimumWidth = 6;
             this.dgvTareas.Name = "dgvTareas";
             // 
-            // label1
+            // lblRegistroActividades
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Teal;
-            this.label1.Location = new System.Drawing.Point(178, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(193, 23);
-            this.label1.TabIndex = 13;
-            this.label1.Text = "Registro de Actividades";
+            this.lblRegistroActividades.AutoSize = true;
+            this.lblRegistroActividades.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegistroActividades.ForeColor = System.Drawing.Color.Teal;
+            this.lblRegistroActividades.Location = new System.Drawing.Point(237, 18);
+            this.lblRegistroActividades.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRegistroActividades.Name = "lblRegistroActividades";
+            this.lblRegistroActividades.Size = new System.Drawing.Size(240, 29);
+            this.lblRegistroActividades.TabIndex = 13;
+            this.lblRegistroActividades.Text = "Registro de Actividades";
             // 
             // cmdIrARegistro
             // 
             this.cmdIrARegistro.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmdIrARegistro.Location = new System.Drawing.Point(182, 359);
+            this.cmdIrARegistro.Location = new System.Drawing.Point(243, 442);
+            this.cmdIrARegistro.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmdIrARegistro.Name = "cmdIrARegistro";
-            this.cmdIrARegistro.Size = new System.Drawing.Size(89, 23);
+            this.cmdIrARegistro.Size = new System.Drawing.Size(119, 28);
             this.cmdIrARegistro.TabIndex = 14;
             this.cmdIrARegistro.Text = "Ir a registro";
             this.cmdIrARegistro.UseVisualStyleBackColor = true;
@@ -104,9 +124,10 @@
             // cmdCancelar
             // 
             this.cmdCancelar.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmdCancelar.Location = new System.Drawing.Point(282, 359);
+            this.cmdCancelar.Location = new System.Drawing.Point(376, 442);
+            this.cmdCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmdCancelar.Name = "cmdCancelar";
-            this.cmdCancelar.Size = new System.Drawing.Size(89, 23);
+            this.cmdCancelar.Size = new System.Drawing.Size(119, 28);
             this.cmdCancelar.TabIndex = 15;
             this.cmdCancelar.Text = "Cancelar";
             this.cmdCancelar.UseVisualStyleBackColor = true;
@@ -114,13 +135,15 @@
             // 
             // frmMostrar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(565, 394);
+            this.ClientSize = new System.Drawing.Size(753, 485);
             this.Controls.Add(this.cmdCancelar);
             this.Controls.Add(this.cmdIrARegistro);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblRegistroActividades);
             this.Controls.Add(this.dgvMostrar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMostrar";
@@ -140,7 +163,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dgvDetalle;
         private System.Windows.Forms.DataGridViewTextBoxColumn dgvReunion;
         private System.Windows.Forms.DataGridViewTextBoxColumn dgvTareas;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRegistroActividades;
         private System.Windows.Forms.Button cmdIrARegistro;
         private System.Windows.Forms.Button cmdCancelar;
     }
